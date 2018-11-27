9bb3392c852b11c8000b9730dfd944ec140f243a
Timothy Tan <timtan93@gmail.com>
2018-11-27 08:56:50 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,11 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-  if phrase != phrase.upcase
-    return "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == phrase.upcase && !'I LOVE YOU GRANDMA!'
+  if phrase == phrase.upcase && phrase != 'I LOVE YOU GRANDMA!'
     return "NO, NOT SINCE 1938!"
   elsif phrase == 'I LOVE YOU GRANDMA!'
     return 'I LOVE YOU TOO PUMPKIN!'
+   else
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
 
